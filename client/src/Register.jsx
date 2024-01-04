412f9aea70f6dddda0540724f39493be77dba6e7
Amit Gupta <144904286+amit-Finnaux@users.noreply.github.com>
2024-01-04 11:57:33 +0530

    first Commit

---
+++ client/src/Register.jsx
@@ -0,0 +1,29 @@
+import { useState } from "react";
+
+export default function Register() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  return (
+    <div className="bg-blue-50 h-screen flex items-center">
+      <form className="w-64 mx-auto">
+        <input
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+          type="text"
+          placeholder="Username"
+          className="block w-full rounded-sm p-2 mb-2 border"
+        />
+        <input
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          type="password"
+          placeholder="Password"
+          className="block w-full rounded-sm p-2 mb-2 border"
+        />
+        <button className="w-full bg-blue-500 text-white rounded-sm p-2">
+          Register
+        </button>
+      </form>
+    </div>
+  );
+}
